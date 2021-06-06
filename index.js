ea6d5ede35eeceb5246acf9ddf2d5934874acbdf
dennis <dsanchez@unpluggedcorp.com>
2021-06-06 17:27:08 -0400

    dependencias y carpeta index

---
+++ index.js
@@ -0,0 +1,38 @@
+//1.importaciones
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const conectarDB = require('./config/db');
+
+require('dotenv').config({
+    path:'.env'
+})
+
+//2.MIDDLEWARE
+app.use(cors());
+//a. conectarnos a la bd
+conectarDB();
+
+app.use(express.json({ extended: true }));
+
+//3.RUTEO
+
+
+
+
+
+
+
+
+
+
+
+
+//4.SERVIDOR
+
+app.listen(4000, () => {
+    console.log("el servidor corriendo");
+})
+
+
+
